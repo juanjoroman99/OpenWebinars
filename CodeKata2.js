c38d9e499c6f3b5ba2ffeeff4f110775d0b0dfd5
Juanjo Roman <rjuanjo563@gmail.com>
2024-12-16 18:50:12 +0100

    CodeKata2 JS

---
+++ CodeKata2.js
@@ -0,0 +1,19 @@
+let randomNumber = 2;
+
+// let price = 233;
+
+// let active = true;
+
+// let sample;
+
+// let sampleNull = null;
+
+// console.log(typeof adress);
+// console.log(typeof price);
+// console.log(typeof active);
+// console.log(typeof sample);
+// console.log(typeof sampleNull);
+
+let boolean = Boolean(randomNumber);
+
+console.log(boolean);
