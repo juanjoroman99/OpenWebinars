32ecf57482a5aaf71e56266504dd607aca8ee796
Juanjo Roman <rjuanjo563@gmail.com>
2025-01-06 22:17:20 +0100

    CodeKata6

---
+++ CodeKtata6.js
@@ -0,0 +1,9 @@
+let hasPizza = 1;
+
+if (hasPizza == false) {
+    console.log("No tengo pizza");
+} else if(hasPizza == true){
+    console.log("Tengo pizza");
+} else {
+    console.log(hasPizza);
+}
